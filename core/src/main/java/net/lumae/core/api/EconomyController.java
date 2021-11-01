1e4c5025d70e4d52c51eba6ddb694e2f9e5e018c
Sam Pizette <samshark23451@gmail.com>
2021-10-31 22:38:43 -0400

    updated-eco-controller

--- core/src/main/java/net/lumae/core/api/EconomyController.java
+++ core/src/main/java/net/lumae/core/api/EconomyController.java
@@ -3,6 +3,8 @@ package net.lumae.core.api;
 import net.lumae.core.economy.Economy;
 import org.bukkit.entity.Player;
 
+import java.util.function.BiConsumer;
+
 public class EconomyController {
     private final Player player;
     private final Economy economy;
@@ -12,26 +14,30 @@ public class EconomyController {
         this.economy = economy;
     }
 
-    public void receipt() {
-        economy.receipt(player);
+    public APIResponse<Player, Void> receipt() {
+        return new APIAction<>(p -> economy.receipt(p), player, null);
     }
 
     //TODO change to double
-    public boolean transfer(Player target, Double amount) {
-        return economy.transfer(player, target, amount.intValue());
+    public APIResponse<Player, Double> transfer(Player target, Double amount) {
+        BiConsumer<Player, Double> biConsumer = (p, amt) -> economy.transfer(player, p, amt);
+        return new APIAction<>(biConsumer, target, amount);
     }
     //TODO change to double
-    public void set(Double amount) {
-        economy.set(player, amount.intValue());
-    }
-
-    //TODO change to double
-    public void deposit(Double amount) {
-        economy.deposit(player, amount.intValue());
+    public APIResponse<Player, Double> set(Double amount) {
+        return new APIAction<>(p -> economy.set(p, amount), player, amount);
     }
 
     //TODO change to double
-    public void withdraw(Double amount) {
+    public APIResponse<Player, Double> deposit(Double amount) {
+        return new APIAction<>(p -> economy.deposit(p, amount), player, amount);
+    }
+
+
+
+    //TODO change to double
+    public APIResponse<Player, Double> withdraw(Double amount) {
         economy.withdraw(player, amount.intValue());
+        return new APIAction<>(p -> economy.withdraw(p, amount), player, amount);
     }
 }
