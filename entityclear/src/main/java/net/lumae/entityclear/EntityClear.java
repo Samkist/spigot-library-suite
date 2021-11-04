10b2daa33b5e198e64ec6fc6ff1f2ab73b83241d
cbotte21 <cbotte21@gmail.com>
2021-11-04 05:11:59 -0400

    implemented-entityclear   - /clear   - plugin.yml

--- entityclear/src/main/java/net/lumae/entityclear/EntityClear.java
+++ entityclear/src/main/java/net/lumae/entityclear/EntityClear.java
@@ -2,10 +2,14 @@ package net.lumae.entityclear;
 
 import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
+import org.jetbrains.annotations.NotNull;
 
 /*
 TODO: Needs functions to be moved out of main for api and command calls.
@@ -13,22 +17,32 @@ TODO: Needs functions to be moved out of main for api and command calls.
 
 import java.util.List;
 
-public class EntityClear extends JavaPlugin {
+public class EntityClear extends JavaPlugin implements CommandExecutor {
     long INTERVAL = 20L;
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (command.getPermission() == "entityclear.clear") {
+            this.clearEntities();
+            return true;
+        }
+        return false;
+    }
+    public void clearEntities() {
+        for (World world : Bukkit.getServer().getWorlds()) {
+            List<Entity> entList = world.getEntities();
+            for(Entity current : entList){
+                if (current.getType() == EntityType.DROPPED_ITEM){
+                    current.remove();
+                }
+            }
+        }
+    }
     @Override
     public void onEnable() {
         BukkitScheduler scheduler = getServer().getScheduler();
         scheduler.scheduleSyncDelayedTask(this, new Runnable() {
             @Override
             public void run() {
-                for (World world : Bukkit.getServer().getWorlds()) {
-                    List<Entity> entList = world.getEntities();
-                    for(Entity current : entList){
-                        if (current.getType() == EntityType.DROPPED_ITEM){
-                            current.remove();
-                        }
-                    }
-                }
+                clearEntities();
             }
         }, this.INTERVAL);
     }
