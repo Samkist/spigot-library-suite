dae9028a285e6c2e88ed76a45dac5ec382e878c6
Samkist <samshark23451@gmail.com>
2022-01-06 17:54:15 -0500

    Moving to beta

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 LumaeMC Plugin Suite
 
 
+The `artifact` folder contains the old code from Sesmester I
+
+This code shows us a lot of things. However I think it should be used purely as idealogical code inspiration and reference. Nothing more
+
+ -- Sam
+
+
---
+++ artifact/core/src/main/java/net/lumae/core/api/EconomyController.java
@@ -0,0 +1,47 @@
+package net.lumae.core.api;
+
+import net.lumae.core.data.util.Pair;
+import net.lumae.core.economy.Economy;
+import org.bukkit.entity.Player;
+
+import java.util.function.BiConsumer;
+
+public class EconomyController {
+    private final Player player;
+    private final Economy economy;
+
+    public EconomyController(Player player, Economy economy) {
+        this.player = player;
+        this.economy = economy;
+    }
+
+    public APIResponse<Player, Void> receipt() {
+        return new APIAction<>(p -> economy.receipt(p), player, null);
+    }
+
+    //TODO change to double
+    public APIResponse<Player, Double> transfer(Player target, Double amount) {
+        BiConsumer<Player, Double> biConsumer = (p, amt) -> economy.transfer(player, p, amt);
+        return new APIAction<>(biConsumer, target, amount);
+    }
+    //TODO change to double
+    public APIResponse<Player, Double> set(Double amount) {
+        return new APIAction<>(p -> economy.set(p, amount), player, amount);
+    }
+
+    //TODO change to double
+    public APIResponse<Player, Double> deposit(Double amount) {
+        return new APIAction<>(p -> economy.deposit(p, amount), player, amount);
+    }
+
+
+
+    //TODO change to double
+    public APIResponse<Player, Double> withdraw(Double amount) {
+        return new APIAction<>(p -> economy.withdraw(p, amount), player, amount);
+    }
+
+    public APIResponse<Pair<Player, Double>, Boolean> attemptWithdraw(Double amount) { //TODO: Better way to implement?
+        return new APIResult<>(p -> economy.attemptWithdraw(p.getKey(), p.getValue()), new Pair<>(player, amount));
+    }
+}
